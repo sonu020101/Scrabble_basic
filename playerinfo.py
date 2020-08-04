b46c117dd586e6d9c27523ae5aa3edb4cb04cd5d
sonu020101 <55920541+sonu020101@users.noreply.github.com>
2020-08-04 09:41:23 +0530

    Update playerinfo.py

--- playerinfo.py
+++ playerinfo.py
@@ -1,41 +1,50 @@
 import letters as l
+"""Stores information of all the players playing"""
 
 players=[]
 class Playerinfo(object):
 
     def __init__(self,name,score):
+        """Define class
+        :param name: str
+        :param score:int
+        :returns None"""
         self.name = name
         self.wordsavail=[]
         self.score=score
     
     def getName(self):
+        """Function that takes input of player names and appends to list
+        :returns appends name to players"""
         self.name=input("Enter player name: ")
         players.append(self.name)
 
     def get_score(self):
+        """Getter function for score
+        :returns int"""
         return self.score
 
     def set_score(self,number):
+        """Setter function for score
+        :returns int"""
         self.number = number
         self.score += self.number
         return self.score
 
     def wordsavail_method(self):
+        """Function that stores all the letters available for each player
+        :param wordsavail: list
+        :returns list"""
         wordsavail=l.Letters.having_letters(self)
         return wordsavail
 
     def playerplaying(self):
+        """Information of playing currently playing
+        :returns player"""
         playing=0
-        while playing<4:
+        while playing<100:
             return player[playing]
             playing+=1
             if playing==4:
                 playing=0
-
-
-
-
-
-
-
 
