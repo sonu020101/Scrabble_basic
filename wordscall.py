29a96875b38825862ed35a6311d633182be39bfd
sonu020101 <55920541+sonu020101@users.noreply.github.com>
2020-08-04 09:40:00 +0530

    Update wordscall.py

--- wordscall.py
+++ wordscall.py
@@ -1,8 +1,8 @@
 class WordsCall(object):
+    """Class that gets all the words available in words.txt"""
     @staticmethod
     def wordread():
         with open ('words.txt') as words:
             wordlist = words.readlines()
             wordlist=[x.strip("\n").casefold() for x in wordlist]
             return wordlist
-
