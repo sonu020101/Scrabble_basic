8bf84f42a4d330bd115742a3177f3eb375f16695
sonu020101 <55920541+sonu020101@users.noreply.github.com>
2020-08-04 21:22:12 +0530

    Update letters.py

--- letters.py
+++ letters.py
@@ -2,10 +2,13 @@ import random
 
 
 class Letters(object):
+    """Getting random letters and reutning them"""
     def __init__(self,owning_letters):
         self.owning_letters=owning_letters
 
     def having_letters(self):
+        """Letters checked and added based on previous word
+        :returns list"""
         self.letter=[chr(c) for c in range(97, 123)]
         self.owning_letters=list()
         i=0
@@ -19,10 +22,9 @@ class Letters(object):
         return self.owning_letters
 
     def skip(self):
+        """If person cant find a word with letters he is having he can can to chane his letter
+        :returns list"""
         self.owning_letters=list()
         for _ in range(7):
             self.owning_letters.append(random.choice(self.letter))
         return self.owning_letters
-
-
-
